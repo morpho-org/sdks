6edd0fb24016da0bd515816582cbff598c85f7d6
Rubilmax <rmilon@gmail.com>
2024-08-13 17:49:13 +0200

    feat(vault-utils): decimals offset

--- packages/blue-sdk/src/vault/VaultUtils.ts
+++ packages/blue-sdk/src/vault/VaultUtils.ts
@@ -1,44 +1,49 @@
 import { MathLib, RoundingDirection } from "../maths";
+import { BigIntish } from "../types";
 
 export namespace VaultUtils {
   export const VIRTUAL_ASSETS = 1n;
 
+  export function decimalsOffset(decimals: BigIntish) {
+    return MathLib.zeroFloorSub(18n, decimals);
+  }
+
   export function toAssets(
-    shares: bigint,
+    shares: BigIntish,
     {
       totalAssets,
       totalSupply,
     }: {
-      totalAssets: bigint;
-      totalSupply: bigint;
+      totalAssets: BigIntish;
+      totalSupply: BigIntish;
     },
-    { decimalsOffset }: { decimalsOffset: bigint },
+    { decimalsOffset }: { decimalsOffset: BigIntish },
     rounding: RoundingDirection = "Down",
   ) {
     return MathLib.mulDiv(
       shares,
-      totalAssets + VIRTUAL_ASSETS,
-      totalSupply + 10n ** decimalsOffset,
+      BigInt(totalAssets) + VIRTUAL_ASSETS,
+      BigInt(totalSupply) + 10n ** BigInt(decimalsOffset),
       rounding,
     );
   }
 
   export function toShares(
-    assets: bigint,
+    assets: BigIntish,
     {
       totalAssets,
       totalSupply,
     }: {
-      totalAssets: bigint;
-      totalSupply: bigint;
+      totalAssets: BigIntish;
+      totalSupply: BigIntish;
     },
-    { decimalsOffset }: { decimalsOffset: bigint },
+    { decimalsOffset }: { decimalsOffset: BigIntish },
     rounding: RoundingDirection = "Up",
   ) {
     return MathLib.mulDiv(
       assets,
-      totalSupply + 10n ** decimalsOffset,
-      totalAssets + VIRTUAL_ASSETS,
+      BigInt(totalSupply) + 10n ** BigInt(decimalsOffset),
+      BigInt(totalAssets) + VIRTUAL_ASSETS,
       rounding,
     );
   }
