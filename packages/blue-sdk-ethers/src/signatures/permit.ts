57491686ef6a5f195a2d8ad39105bcaf6fd6fd8d
Rubilmax <rmilon@gmail.com>
2024-08-20 10:03:37 +0200

    refactor(permit): remove token registry

--- packages/blue-sdk-ethers/src/signatures/permit.ts
+++ packages/blue-sdk-ethers/src/signatures/permit.ts
@@ -2,7 +2,6 @@ import {
   Address,
   ChainId,
   Token,
-  addresses,
   getChainAddresses,
 } from "@morpho-org/blue-sdk";
 
@@ -105,28 +104,3 @@ export const getPermitMessage = (
 
   return getMessage(domain, types, value);
 };
-
-export const WITH_SIMPLE_PERMIT: {
-  [id in ChainId]: (Address | undefined)[];
-} = {
-  [ChainId.EthMainnet]: [
-    addresses[ChainId.EthMainnet].wbIB01,
-    addresses[ChainId.EthMainnet].wbC3M,
-    addresses[ChainId.EthMainnet].wstEth,
-    addresses[ChainId.EthMainnet].sDai,
-    addresses[ChainId.EthMainnet].osEth,
-    addresses[ChainId.EthMainnet].usdc,
-    addresses[ChainId.EthMainnet].dai,
-  ],
-  [ChainId.EthGoerliTestnet]: [
-    "0x0aCd15Fb54034492c392596B56ED415bD07e70d7", // Fake DAI
-    "0xD8134205b0328F5676aaeFb3B2a0DC15f4029d8C", // Real sDAI
-  ],
-  [ChainId.BaseMainnet]: [
-    addresses[ChainId.BaseMainnet].usdc,
-    addresses[ChainId.BaseMainnet].verUsdc,
-  ],
-};
-
-export const hasSimplePermit = (chainId: ChainId, address: Address) =>
-  WITH_SIMPLE_PERMIT[chainId]?.includes(address);
