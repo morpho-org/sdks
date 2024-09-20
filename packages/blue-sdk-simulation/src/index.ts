631591fe38e4bb7c47668fae53079d9624353864
Rubilmax <rmilon@gmail.com>
2024-09-20 13:15:17 +0200

    feat(hooks): export hooks

--- packages/blue-sdk-simulation/src/index.ts
+++ packages/blue-sdk-simulation/src/index.ts
@@ -1,8 +1,10 @@
+export * from "./hooks/index.js";
 export * from "./errors.js";
 export * from "./operations.js";
 export * from "./handlers/index.js";
 export * from "./SimulationState.js";
 
+export * as hooks from "./hooks/index.js";
 export * as errors from "./errors.js";
 export * as operations from "./operations.js";
 export * as handlers from "./handlers/index.js";
