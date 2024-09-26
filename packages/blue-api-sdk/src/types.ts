264a59547c0a29940aa66d6ac328ce0d7fad1e9d
Oumar Fall <o_fall@orange.fr>
2024-09-26 14:54:42 +0200

    fix(blue-api-sdk): fix types

--- packages/blue-api-sdk/src/types.ts
+++ packages/blue-api-sdk/src/types.ts
@@ -1,4 +1,4 @@
-import { MarketId } from "@morpho-org/blue-sdk";
+import { Address, MarketId } from "@morpho-org/blue-sdk";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = {
@@ -26,7 +26,7 @@ export type Scalars = {
   Boolean: { input: boolean; output: boolean };
   Int: { input: number; output: number };
   Float: { input: number; output: number };
-  Address: { input: string; output: string };
+  Address: { input: string; output: Address };
   BigInt: { input: string | number; output: bigint };
   HexString: { input: string; output: string };
   MarketId: { input: string; output: MarketId };
