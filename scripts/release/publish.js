7ed9388b5d67b636eb449a963472f0a7fdd33e48
Rubilmax <rmilon@gmail.com>
2024-10-18 15:08:18 +0200

    ci(publish): divide & conquer

--- scripts/release/publish.js
+++ scripts/release/publish.js
@@ -1,4 +1,4 @@
-import { spawnSync } from "node:child_process";
+import { setOutput } from "@actions/core";
 import { writeChangelogString } from "conventional-changelog-writer";
 import { gt, inc } from "semver";
 import {
@@ -34,80 +34,33 @@ if (releaseType) {
 
   const newTag = `${prefix}v${newVersion}`;
 
-  console.debug(
-    `Version bump from ${version} to ${newVersion} on branch ${branch} (release type: ${releaseType})`,
-  );
-
-  let { stderr, stdout, error } = spawnSync("pnpm", ["version", newVersion], {
-    encoding: "utf8",
-  });
-  if (stdout) console.log(stdout); // Ignore versioning errors.
-  if (error) console.log("pnpm version error:", error);
-  if (stderr) console.log(stderr);
-
-  console.debug(`Publish version ${newVersion} on channel ${channel}`);
-
-  ({ stderr, stdout, error } = spawnSync(
-    "pnpm",
-    ["publish", "--no-git-checks", "--access", "public", "--tag", channel],
-    { encoding: "utf8" },
-  ));
-  if (stdout) console.log(stdout);
-  if (error) {
-    console.error("pnpm publish error:", error);
-    process.exit(1);
-  }
-  if (stderr) {
-    console.error(stderr);
-    process.exit(1);
-  }
-
-  console.debug(`Create release ${newTag}`);
-
-  const createReq = await fetch(
-    "https://api.github.com/repos/morpho-org/sdks/releases",
-    {
-      method: "POST",
-      headers: {
-        Accept: "application/vnd.github+json",
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-        "X-GitHub-Api-Version": "2022-11-28",
+  setOutput("tag", newTag);
+  setOutput("version", newVersion);
+  setOutput("channel", channel);
+  setOutput(
+    "changelog",
+    await writeChangelogString(
+      commits,
+      {
+        version: `[${newTag}](https://github.com/morpho-org/sdks/compare/${tag}...${newTag})`,
+        host: "https://github.com",
+        owner: "morpho-org",
+        repository: "sdks",
+        commit: "commit",
+        types: [
+          { type: "feat", section: "Features" },
+          { type: "fix", section: "Bug Fixes" },
+          { type: "chore", hidden: true },
+          { type: "docs", hidden: true },
+          { type: "style", hidden: true },
+          { type: "refactor", hidden: true },
+          { type: "perf", hidden: true },
+          { type: "test", hidden: true },
+        ],
       },
-      body: JSON.stringify({
-        name: newTag,
-        body: await writeChangelogString(
-          commits,
-          {
-            version: `[${newTag}](https://github.com/morpho-org/sdks/compare/${tag}...${newTag})`,
-            host: "https://github.com",
-            owner: "morpho-org",
-            repository: "sdks",
-            commit: "commit",
-            types: [
-              { type: "feat", section: "Features" },
-              { type: "fix", section: "Bug Fixes" },
-              { type: "chore", hidden: true },
-              { type: "docs", hidden: true },
-              { type: "style", hidden: true },
-              { type: "refactor", hidden: true },
-              { type: "perf", hidden: true },
-              { type: "test", hidden: true },
-            ],
-          },
-          writer,
-        ),
-        tag_name: newTag,
-        target_commitish: branch,
-        prerelease: branch !== "main",
-        draft: false,
-      }),
-    },
+      writer,
+    ),
   );
-
-  if (!createReq.ok) {
-    console.error("github release error:", await createReq.text());
-    process.exit(1);
-  }
-} else console.debug(`No version bump from ${version} on branch ${branch}`);
+}
 
 process.exit(0); // Sometimes hangs.
--- scripts/release/version.js
+++ scripts/release/version.js
@@ -1,8 +1,10 @@
 import { spawnSync } from "node:child_process";
 import { version } from "./bumper.js";
 
-const { stdout } = spawnSync("pnpm", ["version", version], {
-  encoding: "utf8",
-});
+const { stdout } = spawnSync(
+  "pnpm",
+  ["version", version, "--no-git-tag-version"],
+  { encoding: "utf8" },
+);
 
 if (stdout) console.log(stdout); // Ignore versioning errors.
