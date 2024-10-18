f9e851f9aa2c0b6d15e76bd82b8c700f18a55bae
Rubilmax <rmilon@gmail.com>
2024-10-18 10:53:11 +0200

    fix(publish): move to independent js script

---
+++ release.js
@@ -0,0 +1,57 @@
+import { execSync } from "node:child_process";
+import { basename } from "node:path";
+const prefix = `@morpho-org/${basename(process.cwd())}-`;
+const bumper = new Bumper().tag({ prefix });
+
+let { releaseType } = await bumper.bump((commits) => {
+  if (commits.length === 0) return;
+
+  let level = 2;
+
+  commits.forEach((commit) => {
+    if (commit.notes.length > 0) {
+      level = 0;
+    } else if (commit.type === "feat") {
+      if (level === 2) {
+        level = 1;
+      }
+    }
+  });
+
+  return { level };
+});
+
+if (releaseType) {
+  const tag = await bumper.tagGetter();
+  const version = tag.replace(prefix, "");
+
+  const branch = await bumper.gitClient.getCurrentBranch();
+  if (branch !== "main") releaseType = "prerelease";
+
+  const newVersion = inc(
+    version,
+    releaseType,
+    branch !== "main" ? branch : undefined,
+    "0",
+  );
+
+  console.log(execSync(`pnpm version ${newVersion}`, { encoding: "utf8" }));
+
+  console.log(
+    execSync(
+      `pnpm publish --no-git-checks --access public --tag ${branch !== "main" ? branch : "latest"}`,
+      { encoding: "utf8" },
+    ),
+  );
+
+  const newTag = `${prefix}v${newVersion}`;
+
+  console.log(
+    execSync(
+      `gh release create ${newTag} --title ${newTag} ${branch === "main" ? "--latest" : "--prerelease"} --generate-notes --notes-start-tag ${tag}`,
+      { encoding: "utf8" },
+    ),
+  );
+}
+
+process.exit(0);
