057f7d0cbdf21691916b7dfafb7871d26e0a6727
Oumar Fall <o_fall@orange.fr>
2025-01-03 14:45:45 +0100

    fix(permit): use token's domain in permit signature

--- packages/blue-sdk-ethers/src/signatures/permit.ts
+++ packages/blue-sdk-ethers/src/signatures/permit.ts
@@ -27,10 +27,10 @@ export const getPermitMessage = (
 ): SignatureMessage => {
   const { usdc, dai } = getChainAddresses(chainId);
 
-  const domain = {
+  const domain = erc20.eip712Domain ?? {
     name: erc20.name,
     version: erc20.address === usdc ? "2" : "1",
-    chainId: chainId.toString(),
+    chainId,
     verifyingContract: erc20.address,
   };
 
