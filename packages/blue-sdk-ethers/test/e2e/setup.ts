56dcf0481ecd3f81379a48d656fc8879d27ca01a
Rubilmax <rmilon@gmail.com>
2025-01-14 14:36:06 +0100

    docs(position): add deprecated warnings

--- packages/blue-sdk-ethers/test/e2e/setup.ts
+++ packages/blue-sdk-ethers/test/e2e/setup.ts
@@ -1,11 +1,17 @@
 import { createEthersTest } from "@morpho-org/test/vitest/ethers";
 import { mainnet } from "viem/chains";
 
+/**
+ * This test will run on `mainnet` forked at block `19,530,000`.
+ */
 export const test = createEthersTest(mainnet, {
   forkUrl: process.env.MAINNET_RPC_URL,
   forkBlockNumber: 19_530_000,
 });
 
+/**
+ * This test will run on `mainnet` forked at block `21,595,000`.
+ */
 export const test2 = createEthersTest(mainnet, {
   forkUrl: process.env.MAINNET_RPC_URL,
   forkBlockNumber: 21_595_000,
--- packages/blue-sdk-viem/test/setup.ts
+++ packages/blue-sdk-viem/test/setup.ts
@@ -1,11 +1,17 @@
 import { createViemTest } from "@morpho-org/test/vitest";
 import { mainnet } from "viem/chains";
 
+/**
+ * This test will run on `mainnet` forked at block `19,530,000`.
+ */
 export const test = createViemTest(mainnet, {
   forkUrl: process.env.MAINNET_RPC_URL,
   forkBlockNumber: 19_530_000,
 });
 
+/**
+ * This test will run on `mainnet` forked at block `21,595,000`.
+ */
 export const test2 = createViemTest(mainnet, {
   forkUrl: process.env.MAINNET_RPC_URL,
   forkBlockNumber: 21_595_000,
