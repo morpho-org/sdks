454284e68ced21618b85ca9ea250cef9f8ce31e0
Oumar Fall <o_fall@orange.fr>
2025-01-14 11:08:36 +0100

    refactor(migration): add protocol whitelist

--- packages/blue-migration-sdk-viem/src/fetchers/index.ts
+++ packages/blue-migration-sdk-viem/src/fetchers/index.ts
@@ -4,6 +4,7 @@ import type { MigratablePosition } from "../positions/index.js";
 import { MigratableProtocol } from "../types/index.js";
 
 import type { DeploylessFetchParameters } from "@morpho-org/blue-sdk-viem";
+import { fromEntries } from "@morpho-org/morpho-ts";
 import type { Client } from "viem";
 import { fetchAaveV2Positions } from "./aaveV2/aaveV2.fetchers.js";
 import { fetchAaveV3Positions } from "./aaveV3/aaveV3.fetchers.js";
@@ -11,28 +12,42 @@ import { fetchAaveV3OptimizerPositions } from "./aaveV3Optimizer/aaveV3Optimizer
 import { fetchCompoundV2Positions } from "./compoundV2/compoundV2.fetchers.js";
 import { fetchCompoundV3Positions } from "./compoundV3/compoundV3.fetchers.js";
 
+const FETCHERS = {
+  [MigratableProtocol.aaveV2]: fetchAaveV2Positions,
+  [MigratableProtocol.aaveV3]: fetchAaveV3Positions,
+  [MigratableProtocol.aaveV3Optimizer]: fetchAaveV3OptimizerPositions,
+  [MigratableProtocol.compoundV2]: fetchCompoundV2Positions,
+  [MigratableProtocol.compoundV3]: fetchCompoundV3Positions,
+};
+
 export async function fetchMigratablePositions(
   user: Address,
   client: Client,
-  parameters: DeploylessFetchParameters = {},
+  {
+    parameters = {},
+    protocols = [
+      MigratableProtocol.aaveV3Optimizer,
+      MigratableProtocol.aaveV3,
+      MigratableProtocol.aaveV2,
+      MigratableProtocol.compoundV3,
+      MigratableProtocol.compoundV2,
+    ],
+  }: {
+    parameters?: DeploylessFetchParameters;
+    protocols?: MigratableProtocol[];
+  } = {},
 ): Promise<{
   [protocol in MigratableProtocol]?: MigratablePosition[];
 }> {
-  return Promise.all([
-    fetchAaveV3OptimizerPositions(user, client, parameters).then(
-      (positions) => [MigratableProtocol.aaveV3Optimizer, positions] as const,
+  return fromEntries(
+    await Promise.all(
+      protocols.map(
+        async (protocol) =>
+          [
+            protocol,
+            await FETCHERS[protocol](user, client, parameters),
+          ] as const,
+      ),
     ),
-    fetchAaveV3Positions(user, client, parameters).then(
-      (positions) => [MigratableProtocol.aaveV3, positions] as const,
-    ),
-    fetchAaveV2Positions(user, client, parameters).then(
-      (positions) => [MigratableProtocol.aaveV2, positions] as const,
-    ),
-    fetchCompoundV3Positions(user, client, parameters).then(
-      (positions) => [MigratableProtocol.compoundV3, positions] as const,
-    ),
-    fetchCompoundV2Positions(user, client, parameters).then(
-      (positions) => [MigratableProtocol.compoundV2, positions] as const,
-    ),
-  ]).then(Object.fromEntries);
+  );
 }
