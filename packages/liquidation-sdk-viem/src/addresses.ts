4808cd38f2cf64088478e69603c1afbc54d299e7
Rubilmax <rmilon@gmail.com>
2025-02-21 12:35:28 +0100

    fix(liquidation-sdk): add pre liquidation factory configs

--- packages/liquidation-sdk-viem/src/addresses.ts
+++ packages/liquidation-sdk-viem/src/addresses.ts
@@ -95,7 +95,7 @@ export const midasConfigs: Record<ChainId, Record<Address, MidasConfig>> = {
 
 export const preLiquidationFactoryConfigs: Record<
   ChainId,
-  PreLiquidationFactoryConfig
+  PreLiquidationFactoryConfig | undefined
 > = {
   [ChainId.EthMainnet]: {
     address: "0x6FF33615e792E35ed1026ea7cACCf42D9BF83476",
@@ -105,4 +105,6 @@ export const preLiquidationFactoryConfigs: Record<
     address: "0x8cd16b62E170Ee0bA83D80e1F80E6085367e2aef",
     startBlock: 23779056n,
   },
+  [ChainId.ArbitrumMainnet]: undefined,
+  [ChainId.OptimismMainnet]: undefined,
 };
