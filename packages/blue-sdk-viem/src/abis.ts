95ff0645143ab22465dc6e3a0a067b74980c18a1
Rubilmax <rmilon@gmail.com>
2025-04-11 09:31:13 +0200

    test(bundler3): update expected results

--- packages/blue-sdk-viem/src/abis.ts
+++ packages/blue-sdk-viem/src/abis.ts
@@ -525,68 +525,6 @@ export const permit2Abi = [
                 type: "uint48",
               },
             ],
-            internalType: "struct IAllowanceTransfer.PermitDetails[]",
-            name: "details",
-            type: "tuple[]",
-          },
-          {
-            internalType: "address",
-            name: "spender",
-            type: "address",
-          },
-          {
-            internalType: "uint256",
-            name: "sigDeadline",
-            type: "uint256",
-          },
-        ],
-        internalType: "struct IAllowanceTransfer.PermitBatch",
-        name: "permitBatch",
-        type: "tuple",
-      },
-      {
-        internalType: "bytes",
-        name: "signature",
-        type: "bytes",
-      },
-    ],
-    name: "permit",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "owner",
-        type: "address",
-      },
-      {
-        components: [
-          {
-            components: [
-              {
-                internalType: "address",
-                name: "token",
-                type: "address",
-              },
-              {
-                internalType: "uint160",
-                name: "amount",
-                type: "uint160",
-              },
-              {
-                internalType: "uint48",
-                name: "expiration",
-                type: "uint48",
-              },
-              {
-                internalType: "uint48",
-                name: "nonce",
-                type: "uint48",
-              },
-            ],
             internalType: "struct IAllowanceTransfer.PermitDetails",
             name: "details",
             type: "tuple",
