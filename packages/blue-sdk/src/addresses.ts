109e36793698da25338485235c28d7ef884123a9
Oumar Fall <o_fall@orange.fr>
2025-05-21 14:42:12 +0200

    fix(blue-sdk): add croos chain wrapped tokens

--- packages/blue-sdk/src/addresses.ts
+++ packages/blue-sdk/src/addresses.ts
@@ -427,20 +427,44 @@ export const unwrappedTokensMapping: Record<
     [addressesRegistry[ChainId.BaseMainnet].testUsdc]:
       addressesRegistry[ChainId.BaseMainnet].usdc,
   },
-  [ChainId.PolygonMainnet]: {},
+  [ChainId.PolygonMainnet]: {
+    [addressesRegistry[ChainId.PolygonMainnet].wNative]: NATIVE_ADDRESS,
+  },
   [ChainId.ArbitrumMainnet]: {},
-  [ChainId.OptimismMainnet]: {},
-  [ChainId.WorldChainMainnet]: {},
+  [ChainId.OptimismMainnet]: {
+    [addressesRegistry[ChainId.OptimismMainnet].wNative]: NATIVE_ADDRESS,
+  },
+  [ChainId.WorldChainMainnet]: {
+    [addressesRegistry[ChainId.WorldChainMainnet].wNative]: NATIVE_ADDRESS,
+  },
   [ChainId.FraxtalMainnet]: {},
-  [ChainId.ScrollMainnet]: {},
-  [ChainId.InkMainnet]: {},
-  [ChainId.Unichain]: {},
-  [ChainId.SonicMainnet]: {},
-  [ChainId.HemiMainnet]: {},
-  [ChainId.ModeMainnet]: {},
-  [ChainId.CornMainnet]: {},
-  [ChainId.PlumeMainnet]: {},
-  [ChainId.CampMainnet]: {},
+  [ChainId.ScrollMainnet]: {
+    [addressesRegistry[ChainId.ScrollMainnet].wNative]: NATIVE_ADDRESS,
+  },
+  [ChainId.InkMainnet]: {
+    [addressesRegistry[ChainId.InkMainnet].wNative]: NATIVE_ADDRESS,
+  },
+  [ChainId.Unichain]: {
+    [addressesRegistry[ChainId.Unichain].wNative]: NATIVE_ADDRESS,
+  },
+  [ChainId.SonicMainnet]: {
+    [addressesRegistry[ChainId.SonicMainnet].wNative]: NATIVE_ADDRESS,
+  },
+  [ChainId.HemiMainnet]: {
+    [addressesRegistry[ChainId.HemiMainnet].wNative]: NATIVE_ADDRESS,
+  },
+  [ChainId.ModeMainnet]: {
+    [addressesRegistry[ChainId.ModeMainnet].wNative]: NATIVE_ADDRESS,
+  },
+  [ChainId.CornMainnet]: {
+    [addressesRegistry[ChainId.CornMainnet].wNative]: NATIVE_ADDRESS,
+  },
+  [ChainId.PlumeMainnet]: {
+    [addressesRegistry[ChainId.PlumeMainnet].wNative]: NATIVE_ADDRESS,
+  },
+  [ChainId.CampMainnet]: {
+    [addressesRegistry[ChainId.CampMainnet].wNative]: NATIVE_ADDRESS,
+  },
 };
 
 export function getUnwrappedToken(wrappedToken: Address, chainId: ChainId) {
