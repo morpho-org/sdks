4bb25b34a2cf879242b8c97b4d52543ba0e0fc9d
Rubilmax <rmilon@gmail.com>
2025-06-25 12:05:00 +0200

    fix(urls): update subdomain

--- packages/morpho-ts/src/urls.ts
+++ packages/morpho-ts/src/urls.ts
@@ -5,7 +5,7 @@ export const getSubdomainBaseUrl = (subDomain: string) =>
 
 export const CDN_BASE_URL = getSubdomainBaseUrl("cdn");
 export const DOCS_BASE_URL = getSubdomainBaseUrl("docs");
-export const BLUE_API_BASE_URL = getSubdomainBaseUrl("blue-api");
+export const BLUE_API_BASE_URL = getSubdomainBaseUrl("api");
 export const REWARDS_BASE_URL = getSubdomainBaseUrl("rewards");
 export const OPTIMIZERS_BASE_URL = getSubdomainBaseUrl("optimizers");
 export const OPTIMIZERS_API_BASE_URL = getSubdomainBaseUrl("api");
