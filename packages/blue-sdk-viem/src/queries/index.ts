675b90a9ba518df38766e01ff51b6edfcfa1f9cf
Rubilmax <rmilon@gmail.com>
2025-09-30 14:37:46 +0200

    refactor(contracts): rename paths

--- packages/blue-sdk-viem/src/queries/index.ts
+++ packages/blue-sdk-viem/src/queries/index.ts
@@ -2,4 +2,4 @@ export * as GetHolding from "./GetHolding";
 export * as GetMarket from "./GetMarket";
 export * as GetToken from "./GetToken";
 export * as GetVault from "./GetVault";
-export * from "./V2";
+export * from "./vault-v2";
