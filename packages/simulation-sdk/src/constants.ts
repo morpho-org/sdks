bd0101f5f4064acb9d0f86765e4c50c58e0fe4db
foulk <bananox88@gmail.com>
2026-02-23 11:11:16 +0100

    feat(constants): add COMP and FLUID token approval addresses for EthMainnet

--- packages/simulation-sdk/src/constants.ts
+++ packages/simulation-sdk/src/constants.ts
@@ -14,5 +14,7 @@ export const MAX_TOKEN_APPROVALS: Partial<
   [ChainId.EthMainnet]: {
     "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984": maxUint96, // UNI --> see https://github.com/Uniswap/governance/blob/eabd8c71ad01f61fb54ed6945162021ee419998e/contracts/Uni.sol#L154
     "0xfAbA6f8e4a5E8Ab82F62fe7C39859FA577269BE3": maxUint96, // ONDO
+    "0xc00e94Cb662C3520282E6f5717214004A7f26888": maxUint96, // COMP
+    "0x6f40d4A6237C257fff2dB00FA0510DeEECd303eb": maxUint96, // FLUID
   },
 };
