e13521fab565bb5c8f14e2e0bf8748b375891276
Romain (Rubilmax) Milon <romain@morpho.xyz>
2026-05-27 15:47:52 +0200

    chore: exclude test utilities from package outputs

--- packages/blue-sdk-viem/src/fetch/vault-v2/vault-v2.test.ts
+++ packages/blue-sdk-viem/src/fetch/vault-v2/vault-v2.test.ts
@@ -30,7 +30,7 @@ import {
   mockDeploylessRead,
   mockDeploylessReads,
   mockReadFailure,
-} from "../../__test-utils__/viem.js";
+} from "../../__test__/viem.js";
 import {
   blueAbi,
   erc5267Abi,
