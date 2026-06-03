6f00d53f28189cce82ea693bc0c6811bc3f2b0b2
Romain (Rubilmax) Milon <romain@morpho.xyz>
2026-06-03 13:50:24 +0200

    refactor: keep wad inside MathLib

--- packages/morpho-ts/src/constants.test.ts
+++ packages/morpho-ts/src/constants.test.ts
@@ -1,15 +1,10 @@
 import { describe, expect, test } from "vitest";
 
-import { ORACLE_PRICE_SCALE, WAD } from "./constants.js";
-
-describe("WAD", () => {
-  test("default", () => {
-    expect(WAD).toBe(10n ** 18n);
-  });
-});
+import { ORACLE_PRICE_SCALE } from "./constants.js";
+import { MathLib } from "./math.js";
 
 describe("ORACLE_PRICE_SCALE", () => {
   test("default", () => {
-    expect(ORACLE_PRICE_SCALE).toBe(10n ** 36n);
+    expect(ORACLE_PRICE_SCALE).toBe(MathLib.WAD * MathLib.WAD);
   });
 });
