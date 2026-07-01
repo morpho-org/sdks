3157363999349be48828c40b402246a5e23ed5ca
Romain (Rubilmax) Milon <romain@morpho.xyz>
2026-07-01 15:48:49 +0200

    refactor(morpho-sdk): rename midnight bundles requirements

--- packages/morpho-sdk/src/actions/requirements/midnight/index.ts
+++ packages/morpho-sdk/src/actions/requirements/midnight/index.ts
@@ -1,5 +1,5 @@
-export * from "./encodeMidnightPermit2Transfer.js";
+export * from "./encodeMidnightBundlesPermit2Transfer.js";
 export * from "./getMidnightApprovalRequirements.js";
 export * from "./getMidnightAuthorizationRequirement.js";
+export * from "./getMidnightBundlesRequirements.js";
 export * from "./getMidnightRatifyRootRequirement.js";
-export * from "./getMidnightTokenPullRequirements.js";
